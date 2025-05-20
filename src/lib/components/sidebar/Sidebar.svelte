0a893c67960271fc21ccec1a14094d41e36c05af
Gustavo Bordin <gustavo.barreira@belvo.com>
2025-05-20 14:58:55 -0300

    [Changed] styling

--- src/lib/components/sidebar/Sidebar.svelte
+++ src/lib/components/sidebar/Sidebar.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
 	import type { Snippet } from 'svelte';
 
-	type SidebarProps = { children: Snippet };
+	interface Props {
+		children: Snippet;
+	}
 
-	let { children }: SidebarProps = $props();
+	let { children }: Props = $props();
 </script>
 
-<nav class="">
-	{@render children()}
-</nav>
+<aside class="border-r-1 border-border w-[68px] bg-[#1d1f1f]">
+	<nav>
+		<ul class="flex flex-col gap-1 items-center">
+			{@render children()}
+		</ul>
+	</nav>
+</aside>
